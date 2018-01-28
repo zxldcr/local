d7b3b3d17507978b5489ac3729c33f0fefdc90f9
hezi <495556609@qq.com>
2018-01-29 00:13:00 +0800

    导航

---
+++ nav-demo/main.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="zh-Hans">
+<head>
+<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<header></header>
+	<main id = "mainXXXXX">
+	
+	</main>
+	<footer></footer>
+</body>
+<script type="text/javascript">
+	var keys = {
+				'0': {0:'q',1:'w',2:'e',3:'r',4:'t',5:'y',6:'u',7:'i',8:'o',9:'p',length:10},
+				'1': {0:'a',1:'s',2:'d',3:'f',4:'g',5:'h',6:'j',7:'k',8:'l',length:9},
+				'2': {0:'z',1:'x',2:'c',3:'v',4:'b',5:'n',6:'m',length:7},
+				'length': 3
+	}
+	var hash = {'q': 'qq.com', 'w': 'weibo.com', 'e': 'ele.me', 'r': 'renren.com', 't': 'tianya.com', 'y': 'youtube.com', 'u': 'uc.com' , 'i': 'iqiyi.com', 'o': 'opera.com', 'p': undefined, 'a': 'acfun.tv', 's': 'sohu.com', 'z': 'zhihu.com', 'm': 'www.mcdonalds.com.cn'
+	}
+	index = 0;
+	while(index < keys['length']){
+		divXXXXX.createElement('div')
+		mainXXXX.appendChild(divXXXXX)
+		index2 = 0
+		while(index2 < 10){
+			kbdXXXXX.createElement('kbd')
+			kbdXXXXX.textContent('hi')
+			index2 = index2 + 1
+		}
+		index = index + 1
+	}
+
+</script>
+</html>
